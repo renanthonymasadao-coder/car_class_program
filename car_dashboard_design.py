79039cd321b6fb9d2cf0942f80c3abb63883052b
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-05-31 11:57:38 +0800

    adding loading time variables for design

--- car_dashboard_design.py
+++ car_dashboard_design.py
@@ -7,3 +7,12 @@ def intro():
     print("        🚗 SUPER CAR SPEED SIMULATOR 🚗")
     print("=" * 55)
 
+def loading():
+
+    print("\nStarting Engine", end="")
+
+    for i in range(5):
+        print(".", end="", flush=True)
+        time.sleep(0.4)
+
+    print("\nEngine Started Successfully!\n")
