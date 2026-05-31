93a827657edd7ad19edc7f70f4bb4f278b7fe142
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-05-31 12:12:31 +0800

    implementing acceleration and braking system with final touchdown and final result of the program

--- car_class.py
+++ car_class.py
@@ -1,6 +1,48 @@
 # Main Driver Program
 from car import Car
-from dashboard import intro, loading, show_speed
+from car_dashboard_design import intro, loading, show_speed
 import time
+
+# MAIN PROGRAM
+intro()
+loading()
+
+# Create Object
+my_car = Car(2025, "Toyota Supra")
+
+print(f"Car Model : {my_car.get_year_model()}")
+print(f"Car Make  : {my_car.get_make()}\n")
+
+# ACCELERATION
+print("=" * 20)
+print("ACCELERATION MODE")
+print("=" * 20)
+
+for i in range(5):
+
+    my_car.accelerate()
+
+    print(f"\nAcceleration #{i + 1}")
+    show_speed(my_car.get_speed())
+
+    time.sleep(0.7)
+
+# BRAKING
+print("=" * 20)
+print("BRAKE MODE")
+print("=" * 20)
+
+for i in range(5):
+
+    my_car.brake()
+
+    print(f"\nBrake #{i + 1}")
+    show_speed(my_car.get_speed())
+
+    time.sleep(0.7)
+
+print("=" * 55)
+print("        Simulation Complete Successfully!")
+print("=" * 55)
 
 
