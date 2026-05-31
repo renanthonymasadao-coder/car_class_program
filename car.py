8a5a87853aae5da870b3c2d695b284af5c3bed26
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-05-31 11:42:26 +0800

    added constructor code with enscapsulation methods

--- car.py
+++ car.py
@@ -1,1 +1,29 @@
+#Car Class with Encapsulation
 class Car:
+    #CONSTRUCTOR
+    def __init__(self, year_model, make):
+
+        # Private Attributes
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+
+    # METHODS
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
+
+    def get_speed(self):
+        return self.__speed
+
+    def get_make(self):
+        return self.__make
+
+    def get_year_model(self):
+        return self.__year_model
